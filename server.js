8c05d5711983102d9617dce1fef08f5b9e1352eb
satimbaevmuhammad <satimbaevmuhammad3@gmail.com>
2025-05-12 23:11:01 +0500

    done

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+require('dotenv').config();
+
 
 const authRoutes = require('./routes/auth');
 
